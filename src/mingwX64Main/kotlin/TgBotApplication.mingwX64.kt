e342a9a924dfa91fd98e3c4058f6722480850b60
vendelieu <vendelieu@gmail.com>
2025-03-19 16:01:50 +0300

    update

--- src/mingwX64Main/kotlin/TgBotApplication.mingwX64.kt
+++ src/mingwX64Main/kotlin/TgBotApplication.mingwX64.kt
@@ -1,6 +1,6 @@
 import eu.vendeli.tgbot.TelegramBot
 import eu.vendeli.tgbot.generated.__ACTIVITIES
-import eu.vendeli.tgbot.utils.defineActivities
+import eu.vendeli.tgbot.utils.common.defineActivities
 
 actual fun TelegramBot.prepareActivities() {
     defineActivities(__ACTIVITIES)
